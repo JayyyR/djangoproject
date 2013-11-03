767602f234b4fe19d7d0fbf42f77a68d2b5ff4b2
Joe <flatpita92@gmail.com>
2013-11-03 16:42:33 -0500

    hey

--- expenselogger/views.py
+++ expenselogger/views.py
@@ -5,19 +5,28 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, render
 from datetime import datetime
 from django.core.context_processors import csrf
+import pdb
 
 def index(request):
-    expsense_list = []  #implement me
-    context = {'expense_list': expsense_list}
+    expense_list = []  #implement me
+    for e in Expense.objects.raw('SELECT * FROM expenselogger_expense'):
+        expense_list.insert(0, e)
+        print(e.date)
+    
+    for e in expense_list:
+        print(e.amount)
+    context = {'expense_list': expense_list}
     return render(request, 'expenselogger/index.html', context)
     
 def create_expense(request):
-    content = request.POST["expense_name"]
-    amount = request.POST["expense_amount"]
-    date = request.POST["expense_date"]
-    e = Expense(name = content, type = "Flight", amount=amount, date = date)
-    e.save()
-    expsense_list = []  #implement me
-    context = {'expense_list': expsense_list}
-    return render(request, 'expenselogger/index.html', context)
+    try:
+        content = request.POST["expense_name"]
+        amount = request.POST["expense_amount"]
+        date = request.POST["expense_date"]
+        etype = request.POST["expense_type"]
+        e = Expense(name = content, type = etype, amount=amount, date = date)
+        e.save()
+        return index(request)
+    except:
+        return index(request)
 
