bb4a7691a2674ee2df97c002d07207ec70901fc2
Joe <flatpita92@gmail.com>
2013-11-03 21:29:30 -0500

    done

--- expenselogger/tests.py
+++ expenselogger/tests.py
@@ -6,11 +6,20 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from datetime import date
+from expenselogger.models import Expense
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def setUp(self):
+        Expense(name = "Joe", type = "Flight", amount=100, date = date.today())
+        Expense(name = "Bob", type = "Hotel", amount=233, date = date.today())
+
+    def expenses(self):
+        joe = Expense.objects.get(name="Joe")
+        bob = Expense.objects.get(name="Bob")
+        self.assertEqual(joe.name, "Joe")
+        self.assertEqual(joe.type, "Flight")
+        self.assertEqual(joe.amount, 100)
+        self.assertEqual(bob.name, "Bob")
+        self.assertEqual(bob.type, "Hotel")
+        self.assertEqual(bob.amount, 233)
--- expenselogger/views.py
+++ expenselogger/views.py
@@ -1,24 +1,18 @@
 from expenselogger.models import Expense
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
-from django.shortcuts import get_object_or_404, render
-from datetime import datetime
-from django.core.context_processors import csrf
-import pdb
 
 def index(request):
-    expense_list = []  #implement me
-    for e in Expense.objects.raw('SELECT * FROM expenselogger_expense'):
-        expense_list.insert(0, e)
-        print(e.date)
     
-    for e in expense_list:
-        print(e.amount)
+    #grab expenses from database and display them
+    expense_list = []
+    for e in Expense.objects.raw('SELECT * FROM expenselogger_expense'):
+        expense_list.insert(0, e) #insert at top of the list
+
     context = {'expense_list': expense_list}
     return render(request, 'expenselogger/index.html', context)
     
 def create_expense(request):
+    #create an expense and add it to the database
     try:
         content = request.POST["expense_name"]
         amount = request.POST["expense_amount"]
@@ -27,6 +21,7 @@ def create_expense(request):
         e = Expense(name = content, type = etype, amount=amount, date = date)
         e.save()
         return index(request)
+    #if no expense was created, just call main page
     except:
         return index(request)
 
